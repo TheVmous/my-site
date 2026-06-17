5a30d0c8744a5ed7d1b196cb6f1194d7d865e36b
Vmous <vedjoshi2007@gmail.com>
2026-06-17 13:08:34 -0400

    feat: added top nav tab switching capabilities (will need some refactoring later)

--- router.js
+++ router.js
@@ -1,8 +1,29 @@
-
-link = document.getElementById('blog-link');
-link.addEventListener('click', linkClick);
+//TODO: create separate .html pages for each tab and separate .js views too
+const pageDict = {
+    home: "<h1>Home</h1><p>Welcome to Ved's site</p>",
+    blog: "<h1>Blog</h1><p>Incoherent ramblings to be added...</p>",
+    feedback: "<h1>Feedback</h1><p>Compliments? Complaints? Look no further</p>"
+}
+const topnav = document.getElementById('topnav');
+topnav.addEventListener('click', linkClick);
 
 function linkClick(event) {
-    document.getElementById('tmp-output').innerText += "clicked";
+    //console.log("tag clicked: ", event.target.tagName);
+    if (event.target.tagName !== "A") {
+        return;
+    }
+
     event.preventDefault();
+    const targetPage = event.target.dataset.page;
+    console.log(targetPage);
+
+    if (pageDict[targetPage] != 'undefined') {
+        const pageContent = pageDict[targetPage];
+        document.getElementById('app').innerHTML = pageContent;
+    } else {
+        console.log("undefined page");
+        return;
+    }
+
+    
 }
