0f05008818ff32c940ac26d410382ddbb1272d99
Vmous <vedjoshi2007@gmail.com>
2026-07-05 17:05:20 -0700

    fix: fixed form atting for the pages and added admonymous link

--- router.js
+++ router.js
@@ -1,13 +1,15 @@
 //TODO: create separate .html pages for each tab and separate .js views too
+/*
 const pageDict = {
     home: "<h1>Home</h1><p>Welcome to Ved's site</p>",
     blog: "<h1>Blog</h1><p>Incoherent ramblings to be added...</p>",
     feedback: "<h1>Feedback</h1><p>Compliments? Complaints? Look no further</p>"
 }
+*/
 const topnav = document.getElementById('topnav');
 topnav.addEventListener('click', linkClick);
 
-function linkClick(event) {
+async function linkClick(event) {
     if (event.target.tagName !== "A") {
         return;
     }
@@ -16,6 +18,19 @@ function linkClick(event) {
     const targetPage = event.target.dataset.page;
     console.log(targetPage);
 
+    const path = "pages/" + targetPage + ".txt";
+    try {
+        const response = await fetch(path);
+        if (!response.ok) {
+            throw new Error("Target file couldn't be found.");
+        }
+
+        document.getElementById("app").innerHTML = await response.text();
+    } catch {
+        document.getElementById("app").innerHTML = "<h1>Page not found</h1>";
+    }
+
+/*
     if (pageDict[targetPage] != 'undefined') {
         const pageContent = pageDict[targetPage];
         document.getElementById('app').innerHTML = pageContent;
@@ -23,6 +38,6 @@ function linkClick(event) {
         console.log("undefined page");
         return;
     }
-
+*/
     
 }
